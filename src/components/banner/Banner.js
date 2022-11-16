9b35e060fb6d28a0c89872e0b30b813515bac7b2
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-16 15:17:03 +0200

    refactor banner component, created separate styles component

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,18 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
-import colors from '../../utils/colors'
-
-export const BannerContainer = styled.div`
-  width: 100%;
-  height: 200px;
-  background: black;
-  color: ${colors.gainsboro};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 40px;
-  letter-spacing: 3;
-`
+import { BannerContainer } from './Banner.styles'
 
 const Banner = () => {
   return (
---
+++ src/components/banner/Banner.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import colors from '../../utils/colors'
+
+export const BannerContainer = styled.div`
+  width: 100%;
+  height: 200px;
+  background: black;
+  color: ${colors.gainsboro};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  letter-spacing: 3;
+`
