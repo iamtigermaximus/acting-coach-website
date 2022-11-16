4ee3fbfab77d08851d7571a9bac1c9e9c6a9bcfd
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-16 15:22:50 +0200

    refactor Hheader component, created header styles component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,35 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
 import Coach2 from '../../assets/man-g0c4bac179_1920.jpg'
 import Coach3 from '../../assets/man-g96b12eebe_1920.jpg'
 import Coach4 from '../../assets/man-g21569db4d_1920.jpg'
 import Coach5 from '../../assets/man-gae8478dc8_1920.jpg'
-
-export const HeaderContainer = styled.footer`
-  width: 100%;
-  height: 200px;
-  background: black;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 10px;
-  padding-top: 10vh;
-`
-export const BlankForImage = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  flex-direction: row;
-`
-export const Image = styled.img`
-  height: 100%;
-  width: 30%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
+import { HeaderContainer, BlankForImage, Image } from './Header.styles'
 
 const Header = () => {
   return (
---
+++ src/components/header/Header.styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.footer`
+  width: 100%;
+  height: 200px;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 10px;
+  padding-top: 10vh;
+`
+export const BlankForImage = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex-direction: row;
+`
+export const Image = styled.img`
+  height: 100%;
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
